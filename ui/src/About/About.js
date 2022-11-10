2251e6ee52e961e5ecad8cb743600aa175401fe2
Catalyst <103901063+Catalyst4Change@users.noreply.github.com>
2022-11-09 18:26:30 -0700

    install router, create routes

---
+++ ui/src/About/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+
+  return (
+    <section>
+      <h2>About</h2>
+      <p>This app was made from the Hate Symbols Database maintained by the Anti-Defamation League. The ADL fights all forms of antisemitism and bias, using innovation and partnerships to drive impact. A global leader in combating antisemitism, countering extremism and battling bigotry wherever and whenever it happens, ADL works to protect democracy and ensure a just and inclusive society for all.</p>
+      <p>As a patriotic antifascist, I feel it is my duty to educate and inspire my country to recognize the threat posed by white supremacist ideology and organizations. Use this app as a tool to quickly recognize the signs & symbols of increasingly bold hate-groups. You may be unpleasantly surprised how many are present in plain view on any given day.</p>
+    </section>
+  )
+}
+
+export default About
--- ui/src/App/App.js
+++ ui/src/App/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import APIcalls from '../APIcalls/APIcalls.js';
+import { Routes, Route, Link } from 'react-router-dom';
+import About from '../About/About.js';
 import fetchData from '../APIcalls/APIcalls.js';
+import NewGame from '../NewGame/NewGame.js';
 import './App.css';
 
 function App() {
@@ -13,8 +15,28 @@ function App() {
 
   return (
     <div className="App">
+      <header>
+        FASH CARDS
+      </header>
+
+      <div className='logo'>
+        logo
+      </div>
+
+      <nav>
+        <ul>
+          <li><Link to='/about'>About</Link></li>
+          <li><Link to='/newgame'>New Game</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path='/' element={<App/>} />
+        <Route path='/newgame' element={<NewGame />} />
+        <Route path='/about' element={<About path='/about' />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
